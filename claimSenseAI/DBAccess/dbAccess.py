9830392a90fd8685bc2bcd020f5c06c37447d7ce
samuel <samueltsutcliffe@gmail.com>
2025-03-10 12:27:14 +0000

    added the database conection

---
+++ claimSenseAI/DBAccess/dbAccess.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+def db_access():
+    try:
+        print("Attempting connection to the database\n")
+        # put this is your name and password until we're at point where we can insert
+        conn = psycopg2.connect(database = "",
+            user = "samuel",
+            host = "",
+            password = "QwErTy1243!",
+            port = 5432)
+
+        # Creating a cursor with name cur, allows queries to the db
+        cur = conn.cursor()
+        print('Connected to the PostgreSQL database\n')
+        return conn, cur
+    except Exception as e:
+        print("connection was not made. Error: {e}")
+
+db_access()
